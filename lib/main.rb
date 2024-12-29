264088e379184c7843bdda23ab31ca60319f4fa0
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-29 15:58:45 -0800

    Create main.rb to test linked list methods

---
+++ lib/main.rb
@@ -0,0 +1,12 @@
+require_relative 'linked_list'
+
+list = LinkedList.new
+
+list.append('dog')
+list.append('cat')
+list.append('parrot')
+list.append('hamster')
+list.append('snake')
+list.append('turtle')
+
+puts list.tail
