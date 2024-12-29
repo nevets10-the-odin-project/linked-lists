2bb7b6b6094b7d6d6d2f15e7f2f9e7cd48a288c7
Nevets10 <12600557+Nevets10@users.noreply.github.com>
2024-12-29 15:34:08 -0800

    Add method definitions to linked_list

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -4,4 +4,34 @@ class LinkedList
   def initialize
     self.head_node = Node.new
   end
+
+  def append(value)
+  end
+
+  def prepend(value)
+  end
+
+  def size
+  end
+
+  def head
+  end
+
+  def tail
+  end
+
+  def at(index)
+  end
+
+  def pop
+  end
+
+  def contains?(value)
+  end
+
+  def find(value)
+  end
+
+  def to_s
+  end
 end
